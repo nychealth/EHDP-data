353fa21120acac2a28fc40ec4bd3cac439e301a1
Christopher Gettings <gettings.chris@gmail.com>
2022-09-13 16:37:45 -0400

    preventing overwhelming console output

--- export-scripts/NR_SparkBarExport.py
+++ export-scripts/NR_SparkBarExport.py
@@ -8,13 +8,30 @@
 import pandas as pd
 import altair as alt
 from altair_saver import save
+import os
+import warnings
+from selenium import webdriver
+
+# prevent 4000+ lines of console output
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--log-level=3')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(options = options)
+
+# prevent other warnings
+
+warnings.simplefilter("ignore")
+
+cwd = os.getcwd()
 
 data_files = [
-    "Housing and Health _data.csv", 
-    "Outdoor Air and Health_data.csv",
-    "Active Design Physical Activity and Health_data.csv", 
-    "Asthma and the Environment_data.csv", 
-    "Climate and Health_data.csv"
+    "Housing_and_Health_data.csv",
+    "Outdoor_Air_and_Health_data.csv",
+    "Active_Design_Physical_Activity_and_Health_data.csv",
+    "Asthma_and_the_Environment_data.csv",
+    "Climate_and_Health_data.csv"
 ]
 
 # looping through files
@@ -23,7 +40,8 @@ for file in data_files:
 
     print(file)
 
-    df = pd.read_csv("neighborhood-reports/data/" + file)
+    # df = pd.read_csv(cwd + "/neighborhood-reports/data/" + file)
+    df = pd.read_csv(cwd + "/../neighborhood-reports/data/" + file)
 
     # convert End Date to date data type
 
@@ -92,7 +110,7 @@ for file in data_files:
 
         # - name each SVG with the data_field_name and the Neighborhood
         
-        image_name = 'neighborhood-reports/images/' + df['data_field_name'][ind] + '_' + df['geo_join_id'][ind] + '.svg'
+        image_name = cwd + '/../neighborhood-reports/images/' + df['data_field_name'][ind] + '_' + df['geo_join_id'][ind] + '.svg'
 
         save(chart, fp = image_name)
         
