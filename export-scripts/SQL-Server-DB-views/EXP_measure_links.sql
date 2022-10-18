08cb062a3aed8fb36ea1a4ac98593590a02e1b9e
Christopher Gettings <gettings.chris@gmail.com>
2022-10-18 11:01:25 -0400

    converting tabs to spaces

--- export-scripts/SQL-Server-DB-views/EXP_measure_links.sql
+++ export-scripts/SQL-Server-DB-views/EXP_measure_links.sql
@@ -28,8 +28,8 @@ ALTER VIEW [dbo].[EXP_measure_links] AS
 
         LEFT JOIN subtopic_indicators AS si1 ON si1.indicator_id = i_to_i.base_indicator_id
         LEFT JOIN subtopic_indicators AS si2 ON si2.indicator_id = i_to_i.linked_indicator_id
-		LEFT JOIN subtopic            AS st1 ON st1.subtopic_id  = si1.subtopic_id
-		LEFT JOIN subtopic            AS st2 ON st2.subtopic_id  = si2.subtopic_id
+        LEFT JOIN subtopic            AS st1 ON st1.subtopic_id  = si1.subtopic_id
+        LEFT JOIN subtopic            AS st2 ON st2.subtopic_id  = si2.subtopic_id
 
     -- removing base or linked Measures whose subtopics aren't publicly displayed. If 
     --  a measure is part of 2 subtopics, one of which is public, then the measure will
@@ -39,9 +39,9 @@ ALTER VIEW [dbo].[EXP_measure_links] AS
 
     WHERE 
         -- disparity_flag != 1 AND
-		st1.public_display_flag = 'Y' AND
-		st2.public_display_flag = 'Y' AND
-		si1.push_ready = 1 AND
-		si2.push_ready = 1
+        st1.public_display_flag = 'Y' AND
+        st2.public_display_flag = 'Y' AND
+        si1.push_ready = 1 AND
+        si2.push_ready = 1
 
 GO
--- export-scripts/SQL-Server-DB-views/ReportPublicList.sql
+++ export-scripts/SQL-Server-DB-views/ReportPublicList.sql
@@ -4,15 +4,15 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 ALTER VIEW [dbo].[ReportPublicList] AS
-		
-	SELECT
-		report_id,
-		replace(title, ',', '') AS title,
-		report_description,
-		public_flag
-	FROM
-		dbo.report
-	WHERE
-		public_flag = 1
-			
+        
+    SELECT
+        report_id,
+        replace(title, ',', '') AS title,
+        report_description,
+        public_flag
+    FROM
+        dbo.report
+    WHERE
+        public_flag = 1
+            
 GO
