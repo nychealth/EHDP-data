e71807a03c16bfb8c2a4b6fd5e602af863ac18bf
Christopher Gettings <gettings.chris@gmail.com>
2022-12-14 12:24:41 -0500

    removing merge error

--- export-scripts/SQL-Server-DB-views/EXP_data_export.sql
+++ export-scripts/SQL-Server-DB-views/EXP_data_export.sql
@@ -6,8 +6,6 @@ GO
 ALTER VIEW [dbo].[EXP_data_export] AS
     
     SELECT DISTINCT
-    
-    SELECT DISTINCT
 
         -- "DISTINCT" because an indicator can have multiple subtopics, but we have no subtopic data here
     
@@ -22,11 +20,7 @@ ALTER VIEW [dbo].[EXP_data_export] AS
         mt.number_decimal_ind,
 
         -- null out value when show data = 0
-        -- null out value when show data = 0
 
-        CASE WHEN un.show_data_flag = 1 THEN ind.data_value 
-            ELSE null 
-        END AS Value,
         CASE WHEN un.show_data_flag = 1 THEN ind.data_value 
             ELSE null 
         END AS Value,
@@ -46,13 +40,7 @@ ALTER VIEW [dbo].[EXP_data_export] AS
         END AS Note
 
     FROM dbo.indicator_data AS ind
-    FROM dbo.indicator_data AS ind
 
-        INNER JOIN dbo.subtopic_indicators AS si ON (
-            si.indicator_id = ind.indicator_id AND 
-            si.geo_type_id  = ind.geo_type_id AND 
-            si.year_id      = ind.year_id 
-        )
         INNER JOIN dbo.subtopic_indicators AS si ON (
             si.indicator_id = ind.indicator_id AND 
             si.geo_type_id  = ind.geo_type_id AND 
@@ -65,19 +53,9 @@ ALTER VIEW [dbo].[EXP_data_export] AS
         INNER JOIN dbo.geo_type             AS gt ON gt.geo_type_id         = ind.geo_type_id
         INNER JOIN dbo.indicator_year       AS iy ON iy.year_id             = ind.year_id
         INNER JOIN dbo.unreliability        AS un ON un.unreliability_id    = ind.unreliability_flag
-        INNER JOIN dbo.subtopic             AS st ON st.subtopic_id         = si.subtopic_id
-        INNER JOIN dbo.internal_indicator   AS ii ON ii.internal_id         = si.internal_id
-        INNER JOIN dbo.measurement_type     AS mt ON mt.measurement_type_id = si.measurement_type_id
-        INNER JOIN dbo.geo_type             AS gt ON gt.geo_type_id         = ind.geo_type_id
-        INNER JOIN dbo.indicator_year       AS iy ON iy.year_id             = ind.year_id
-        INNER JOIN dbo.unreliability        AS un ON un.unreliability_id    = ind.unreliability_flag
 
     -- only export data flagged for public view
-    -- only export data flagged for public view
 
-    WHERE 
-        st.public_display_flag = 'Y' AND
-        si.push_ready = 1
     WHERE 
         st.public_display_flag = 'Y' AND
         si.push_ready = 1
