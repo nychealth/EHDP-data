582bd7511145ddfaee91bf77bfd2c2d058d3ed50
Christopher Gettings <gettings.chris@gmail.com>
2023-01-11 14:17:39 -0500

    adding comparisons metadata

--- export-scripts/EXP_indicator_metadata_writer.py
+++ export-scripts/EXP_indicator_metadata_writer.py
@@ -187,22 +187,33 @@ measure_trend = (
     .rename(columns = {0: "Trend"})
 )
 
-# ==== measure (non-boro) comparisons ==== #
 
-# ---- measure (non-boro) comparisons ---- #
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# measure (non-boro) trend comparisons
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+# ==== specific comparisons view ==== #
 
 EXP_measure_comparisons = (
     pd.read_sql("SELECT * FROM EXP_measure_comparisons", EHDP_odbc)
     .sort_values(by = ["IndicatorID", "ComparisonID", "MeasureID"])
+    .dropna()
 )
 
-# ---- measure (non-boro) comparisons ---- #
+
+# ==== nesting ComparisonIDs ==== #
 
 indicator_comparisons = (
     EXP_measure_comparisons
     .loc[:, ["IndicatorID", "ComparisonID"]]
     .drop_duplicates()
+    .dropna()
+    .groupby(["IndicatorID"])
+    .apply(lambda x: x["ComparisonID"].tolist()) # [] returns Series, not DataFrame
+    .reset_index()
+    .rename(columns = {0: "Comparisons"})
 )
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 # linked measures
@@ -384,6 +395,10 @@ metadata = (
     ]].to_dict('records'))
     .reset_index()
     .rename(columns = {0: "Measures"})
+    .merge(
+        indicator_comparisons,
+        how = "left"
+    )
 )
 
 
