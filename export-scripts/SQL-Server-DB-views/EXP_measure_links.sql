fbf61b51833e8f7ea3aff6230b8a1605cf78871a
Christopher Gettings <cgettings@health.nyc.gov>
2023-02-10 11:50:01 -0500

    efining links

--- export-scripts/SQL-Server-DB-views/EXP_measure_links.sql
+++ export-scripts/SQL-Server-DB-views/EXP_measure_links.sql
@@ -9,25 +9,26 @@ ALTER VIEW [dbo].[EXP_measure_links] AS
 
         /* for debugging / sanity check: */
 
-        -- i_to_i.base_indicator_id,
-        -- i_to_i.linked_indicator_id,
+        -- ii.base_indicator_id,
+        -- ii.linked_indicator_id,
         -- st1.subtopic_id AS st1,
         -- st2.subtopic_id AS st2,
         -- st1.public_display_flag AS pdf1,
-        -- st2.public_display_flag AS pdf2
+        -- st2.public_display_flag AS pdf2,
 
         base_indicator_id   AS BaseMeasureID,
         linked_indicator_id AS MeasureID,
+        -- ii.disparity_flag,
 
         CASE x_axis 
             WHEN 1 THEN 'x'
             WHEN 0 THEN 'y'
         END AS SecondaryAxis
 
-    FROM i_to_i 
+    FROM i_to_i AS ii 
 
-        LEFT JOIN subtopic_indicators AS si1 ON si1.indicator_id = i_to_i.base_indicator_id
-        LEFT JOIN subtopic_indicators AS si2 ON si2.indicator_id = i_to_i.linked_indicator_id
+        LEFT JOIN subtopic_indicators AS si1 ON si1.indicator_id = ii.base_indicator_id
+        LEFT JOIN subtopic_indicators AS si2 ON si2.indicator_id = ii.linked_indicator_id
         LEFT JOIN subtopic            AS st1 ON st1.subtopic_id  = si1.subtopic_id
         LEFT JOIN subtopic            AS st2 ON st2.subtopic_id  = si2.subtopic_id
 
