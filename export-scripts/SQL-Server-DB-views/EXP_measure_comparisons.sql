8734554f4b6dc3dcc609995ed3e7fe5e98688ff0
Christopher Gettings <cgettings@health.nyc.gov>
2023-02-22 16:05:21 -0500

    updating measure copy

--- export-scripts/SQL-Server-DB-views/EXP_measure_comparisons.sql
+++ export-scripts/SQL-Server-DB-views/EXP_measure_comparisons.sql
@@ -9,11 +9,10 @@ ALTER VIEW dbo.EXP_measure_comparisons AS
         mc.measure_compare_id  AS ComparisonID,
         mc.name                AS ComparisonName,
         mc.group_title_display AS LegendTitle,
-        mc.Y_axis_title,
         idef.internal_id       AS IndicatorID,
         mm.indicator_id        AS MeasureID
 
-    FROM measure_compare AS mc
+    FROM measure_compare_copy AS mc
         LEFT JOIN m_to_m               AS   mm ON mm.measure_compare_id = mc.measure_compare_id
         LEFT JOIN indicator_definition AS idef ON idef.indicator_id     = mm.indicator_id
 
