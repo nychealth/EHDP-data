dd7e7ec2963a5df0f7959de76c1ac2cb9cdd4298
Christopher Gettings <cgettings@health.nyc.gov>
2023-02-24 13:07:36 -0500

    updating measure copy

--- export-scripts/EXP_measure_comparisons_writer.R
+++ export-scripts/EXP_measure_comparisons_writer.R
@@ -146,7 +146,7 @@ EXP_measure_comparisons <-
 
 comparisons_nested <- 
     EXP_measure_comparisons %>% 
-    drop_na() %>% 
+    # drop_na() %>% 
     mutate(ComparisonName = ComparisonName %>% str_remove_all("<.*?>")) %>% # remove HTML tags
     rename(Measures = MeasureID) %>% 
     group_by(ComparisonID, ComparisonName, LegendTitle, Y_axis_title, IndicatorID) %>% 
