d0683ff75ca7d9f0275002dfb0cc177e7a0d93a0
Christopher Gettings <gettings.chris@gmail.com>
2023-04-07 08:52:26 -0400

    switching back to threads preference

--- export-scripts/NR_SparkBarExport.py
+++ export-scripts/NR_SparkBarExport.py
@@ -51,8 +51,8 @@ def chart_fun(ind, df, base_dir, conda_prefix):
 
     # - filter by data field name to create dataset
 
-    dset = df[df.data_field_name == df['data_field_name'][ind]]
-    dset = dset.sort_values('data_value')
+    dset = df[df.indicator_data_name == df['indicator_data_name'][ind]]
+    dset = dset.sort_values('data_value_geo_entity')
 
     # - use Altair, the python connector to Vega-Lite
     # - https://altair-viz.github.io/getting_started/overview.html
@@ -62,7 +62,7 @@ def chart_fun(ind, df, base_dir, conda_prefix):
             .mark_bar()
             .encode(
                 x = alt.X('neighborhood', sort = 'y', axis = None),
-                y = alt.Y('data_value', axis = None),
+                y = alt.Y('data_value_geo_entity', axis = None),
                 
                 # The highlight will be set on the result of a conditional statement
                 
@@ -88,9 +88,9 @@ def chart_fun(ind, df, base_dir, conda_prefix):
 
     chart_json = chart.to_json()
 
-    # - name each SVG with the data_field_name and the Neighborhood
+    # - name each SVG with the indicator_data_name and the Neighborhood
 
-    image_name = base_dir + '/neighborhood-reports/images/' + df['data_field_name'][ind] + '_' + df['geo_join_id'][ind] + '.svg'
+    image_name = base_dir + '/neighborhood-reports/images/' + df['indicator_data_name'][ind] + '_' + df['geo_join_id'][ind] + '.svg'
 
     # use VL CLI program to create SVG
 
@@ -137,26 +137,26 @@ for file in data_files:
     # - only the most recent data End Date for each data field
     
     df = df.sort_values('end_date')
-    df = df.drop_duplicates(subset = ['data_field_name', 'geo_join_id'], keep = 'last')
+    df = df.drop_duplicates(subset = ['indicator_data_name', 'geo_join_id'], keep = 'last')
     df = df.sort_values('geo_join_id')
     
     # - converted the integer to string so that I can concatenate to an image title later
     
     df['geo_join_id'] = df['geo_join_id'].astype(str)
     
-    # Step three: for each data_field_name in the data frame, create a graph and write to SVG file
-    # - create a list of distinct data_field_name / Neighborhood s,
+    # Step three: for each indicator_data_name in the data frame, create a graph and write to SVG file
+    # - create a list of distinct indicator_data_name / Neighborhood s,
     
     df = pd.DataFrame(
         df, 
-        columns = ['data_field_name', 'neighborhood', 'data_value', 'geo_join_id']
+        columns = ['indicator_data_name', 'neighborhood', 'data_value_geo_entity', 'geo_join_id']
     )
     
     # - then loop through the list
 
     results = Parallel(
         n_jobs = cpus, 
-        # prefer = "threads", 
-        verbose = 1
+        prefer = "threads", 
+        verbose = 2
     )(delayed(chart_fun)(ind, df, base_dir, conda_prefix) for ind in df.index)
 
