d121f8b52963b4bb22da32fa9d54338ecbe14be9
cgettings <cgettings@health.nyc.gov>
2023-05-15 15:53:51 -0400

    Update syndromic script

--- export-scripts/syndromic_push.r
+++ export-scripts/syndromic_push.r
@@ -2,15 +2,19 @@
 
 # load reader, for better file reading
 library(readr)
+library(gert)
 
 # fetch info on all changes in remote repo
-system("git fetch origin")
+git_fetch("origin")
+# system("git fetch origin")
 
 # make sure you're on the production branch
-system("git checkout production")
+git_branch_checkout("production")
+# system("git checkout production")
 
 # pull all changes on production
-system("git pull --all")
+git_pull("origin")
+# system("git pull --all")
 
 # set long file path in object
 heat_syndrome_dir <- "~/networkDrives/smb-share:server=sasshare01,share=sasshare/EHS/BESP/SecuredFolder/Syndromic/Heat_ED/EH data portal/live_data/EHDP-data/datafeatures/heatsyndrome"
@@ -24,11 +28,14 @@ edheat_live2 <- edheat_live[edheat_live$END_DATE > start & edheat_live$END_DATE 
 # save updated data to repo
 write_csv(edheat_live2, "~/EHDP-data/key-topics/heat-syndrome/edheat2023_live.csv")
 
-# add the file changes
-system("git add --all")
+# add all file changes
+git_add(".")
+# system("git add .")
 
 # commit with message
-system("git commit -m -a 'Regular auto-commit'")
+git_commit_all("Regular auto-commit")
+# system("git commit -m 'Regular auto-commit' -a")
 
 # push changes to production
-system("git push origin production")
+git_push("origin")
+# system("git push origin")
