d47baaa68d3a3bc398274c28fffe129fe5b9d36e
cgettings <cgettings@health.nyc.gov>
2023-05-25 16:11:34 -0400

    working dir

--- export-scripts/syndromic_push.r
+++ export-scripts/syndromic_push.r
@@ -20,8 +20,6 @@ git_branch_checkout("production")
 # system("git pull --all")
 git_pull("origin")
 
-setwd("/home/health.dohmh.nycnet/klane1/EHDP-data")
-
 # set long file path in object
 heat_syndrome_dir <- "~/networkDrives/smb-share:server=sasshare01,share=sasshare/EHS/BESP/SecuredFolder/Syndromic/Heat_ED/EH data portal/live_data/EHDP-data/datafeatures/heatsyndrome"
 
