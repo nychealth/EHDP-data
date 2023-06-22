f1a8d4e2994e8a1fbabce40b74d2260de350a13c
cgettings <cgettings@health.nyc.gov>
2023-06-22 11:17:34 -0400

    use CREATE OR ALTER VIEW

--- export-scripts/SQL-Server-DB-views/EXP_measure_comparisons.sql
+++ export-scripts/SQL-Server-DB-views/EXP_measure_comparisons.sql
@@ -3,7 +3,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER VIEW dbo.EXP_measure_comparisons AS
+CREATE OR ALTER VIEW dbo.EXP_measure_comparisons AS
 
     SELECT
         mc.measure_compare_id  AS ComparisonID,
