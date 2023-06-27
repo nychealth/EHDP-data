72a78a5637aa059fde95b7b194b8df821f568750
cgettings <cgettings@health.nyc.gov>
2023-06-27 14:57:17 -0400

    Revert "Regular auto-commit"
    This reverts commit 92901c648abe461386aabc3066ec5dcf5d2ce8f0.

--- export-scripts/create_GeoLookup.R
+++ export-scripts/create_GeoLookup.R
@@ -126,7 +126,8 @@ EHDP_odbc <-
         server = "SQLIT04A",
         database = db_name,
         trusted_connection = "yes",
-        encoding = "latin1"
+        encoding = "latin1",
+        trustservercertificate = "yes"
     )
 
 
@@ -495,6 +496,23 @@ nyc_kids_2021 <-
     arrange(GeoID)
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# harbor areas
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+ny_harbor <- 
+    read_sf("geography/ny_harbor.topo.json", crs = st_crs(4326)) %>% 
+    st_transform(st_crs(2263)) %>% 
+    mutate(center = st_centroid(geometry)) %>% 
+    as_tibble() %>% 
+    transmute(
+        GeoType = "NYHarbor",
+        GeoID = GEOCODE,
+        Lat = st_coordinates(st_transform(center, st_crs(4326)))[, 2],
+        Long = st_coordinates(st_transform(center, st_crs(4326)))[, 1]
+    ) %>%  
+    arrange(GeoID)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 # row-binding
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
@@ -513,7 +531,8 @@ all_geos <-
         nyc_kids_nodate,
         nyc_kids_2017,
         nyc_kids_2019,
-        nyc_kids_2021
+        nyc_kids_2021,
+        ny_harbor
     ) %>% 
     mutate(roworder = 1:nrow(.))
 
