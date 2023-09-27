035a10d02613160e056a55fedd5661f400093d76
cgettings <gettings.chris@gmail.com>
2023-09-26 23:33:56 -0400

    updating DWQ trend etc.

--- export-scripts/EXP_indicator_data_writer.R
+++ export-scripts/EXP_indicator_data_writer.R
@@ -145,8 +145,8 @@ EHDP_odbc <-
         server = server,
         database = db_name,
         trusted_connection = "yes",
-        # encoding = "utf8",
-        encoding = "latin1",
+        encoding = "utf8",
+        # encoding = "latin1",
         trustservercertificate = "yes"
     )
 
