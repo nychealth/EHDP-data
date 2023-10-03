2ba0d22b5b378718592a2ecf1f80fd7a81725e80
cgettings <gettings.chris@gmail.com>
2023-10-03 14:33:53 -0400

    database logic upgrades

--- export-scripts/run_all_export_scripts.ps1
+++ export-scripts/run_all_export_scripts.ps1
@@ -33,20 +33,164 @@ if ($Env:COMPUTERNAME -eq "DESKTOP-PU7DGC1") {
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-# Choose database to use
+    # make sure you're on the right branch
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-
-$Env:data_env = (Read-Host "staging [s] or production [p]?").ToLower().Trim()[0]
-
-# make sure you're on the right branch
 
 $current_branch = git branch --show-current
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# Choose database to use
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+if (!$Env:data_env) {
+
+    # if $Env:data_env doesn't exist yet, ask about setting it
+
+    Write-Host "-------------------------------------------------------------"
+    Write-Host ">> 1: `$Env:data_env = []"
+
+    Write-Host "-------------------------------------------------------------"
+    $Env:data_env = (Read-Host "staging [*s] or production [p]? ").ToLower().Trim()[0]
+
+    # if nothing entered, default to staging
+
+    if (!$Env:data_env) {
+
+        Write-Host "-------------------------------------------------------------"
+        Write-Host ">> 2: `$Env:data_env = [], default to s"
+        $Env:data_env = "s"
+
+    }
+
 if (($Env:data_env -eq "s") -and ($current_branch -ne "staging")) {
 
-    $switch = (Read-Host "Switch to staging? Yes [y] / No [n]").ToLower().Trim()[0]
+        # ask about switching
+
+        Write-Host "-------------------------------------------------------------"
+        Write-Host ">> 3: `$Env:data_env = s, not on < staging > branch"
+
+        Write-Host "-------------------------------------------------------------"
+        $switch = (Read-Host "Switch to < staging >? Yes [y] / No [*n] ").ToLower().Trim()[0]
+
+        # switch branch, or not
+
+        if ($switch -eq "y") {
+
+            Write-Host "-------------------------------------------------------------"
+            Write-Host ">> 4: `$switch = y, switching to < staging > branch"
+
+            git checkout staging
+            git pull
+
+        } elseif ($switch -eq "n") {
+
+            # don't switch
+
+            Write-Host "-------------------------------------------------------------"
+            Write-Host ">> 4: `$switch = n, staying on < $current_branch > branch"
+
+        } else {
+
+            # don't switch
+
+            Write-Host "-------------------------------------------------------------"
+            Write-Host ">> 4: `$switch = [], staying on < $current_branch > branch"
+
+        }
+
+        
+    } elseif (($Env:data_env -eq "p") -and ($current_branch -ne "production")) {
+
+        # ask about switching
+
+        Write-Host "-------------------------------------------------------------"
+        Write-Host ">> 3: `$Env:data_env = p, not on < production > branch"
+
+        Write-Host "-------------------------------------------------------------"
+        $switch = (Read-Host "Switch to < production > ? Yes [y] / No [*n] ").ToLower().Trim()[0]
+
+        # switch branch, or not
+
+        if ($switch -eq "y") {
+
+            Write-Host "-------------------------------------------------------------"
+            Write-Host ">> 4: `$switch = y, switching to < production > branch"
+
+            git checkout staging
+            git pull
+
+        } elseif ($switch -eq "n") {
+
+            # don't switch
+
+            Write-Host "-------------------------------------------------------------"
+            Write-Host ">> 4: `$switch = n, staying on < $current_branch > branch"
+
+        } else {
+
+            # don't switch
+
+            Write-Host "-------------------------------------------------------------"
+            Write-Host ">> 4: `$switch = [], staying on < $current_branch > branch"
+
+        }
+        
+
+    } else {
+        
+        # stay on this branch
+
+        Write-Host "-------------------------------------------------------------"
+        Write-Host ">> 3: on < $current_branch >"
+
+    }
+
+
+} else {
+
+    # if the $Env:data_env does exist, ask about setting it
+
+    Write-Host "-------------------------------------------------------------"
+    Write-Host ">> 1: '`$Env:data_env' exists"
+
+    # if the $Env:data_env does exist, ask about chanaging it
+
+    Write-Host "-------------------------------------------------------------"
+    $switch = (Read-Host "`$Env:data_env = $Env:data_env ... Switch environment? Yes [y] / No [*n] ").ToLower().Trim()[0]
+
+    # change environment by overwriting $Env:data_env
 
     if ($switch -eq "y") {
+
+        Write-Host "-------------------------------------------------------------"
+        Write-Host ">> 2: switch '`$Env:data_env'"
+
+        Write-Host "-------------------------------------------------------------"
+        $Env:data_env = (Read-Host "staging [*s] or production [p]? ").ToLower().Trim()[0]
+
+        # if nothing entered, default to staging
+
+        if (!$Env:data_env) {
+            
+            Write-Host "-------------------------------------------------------------"
+            Write-Host ">> 3: `$Env:data_env = [], default to s"
+            $Env:data_env = "s"
+        
+        }
+
+
+        if (($Env:data_env -eq "s") -and ($current_branch -ne "staging")) {
+
+            Write-Host "-------------------------------------------------------------"
+            Write-Host ">> 4: `$Env:data_env = s, not on staging branch"
+
+            Write-Host "-------------------------------------------------------------"
+            $switch = (Read-Host "Switch to staging? Yes [y] / No [*n] ").ToLower().Trim()[0]
+
+            if ($switch -eq "y") {
+
+                Write-Host "-------------------------------------------------------------"
+                Write-Host ">> 5: switching to staging branch"
 
         git checkout staging
         git pull
@@ -56,16 +200,62 @@ if (($Env:data_env -eq "s") -and ($current_branch -ne "staging")) {
     
 } elseif (($Env:data_env -eq "p") -and ($current_branch -ne "production")) {
 
-    $switch = (Read-Host "Switch to production? Yes [y] / No [n]").ToLower().Trim()[0]
+            Write-Host "-------------------------------------------------------------"
+            Write-Host ">> 4: `$Env:data_env = p, not on < production > branch"
 
-    if ($switch -eq "y") {
+            Write-Host "-------------------------------------------------------------"
+            $switch = (Read-Host "Switch to < production > ? Yes [y] / No [*n] ").ToLower().Trim()[0]
+
+            if ($switch -eq "y") {
+
+                Write-Host "-------------------------------------------------------------"
+                Write-Host ">> 5: switching to < production > branch"
 
         git checkout production
         git pull
 
+            } elseif ($switch -eq "n") {
+
+                # don't switch
+
+                Write-Host "-------------------------------------------------------------"
+                Write-Host ">> 5: `$switch = n, staying on < $current_branch > branch"
+
+            } else {
+
+                # don't switch
+
+                Write-Host "-------------------------------------------------------------"
+                Write-Host ">> 5: `$switch = [], staying on < $current_branch > branch"
+
+            }
+            
+        } else {
+
+            Write-Host "-------------------------------------------------------------"
+            Write-Host ">> 4: staying on < $current_branch > branch"
+
+        }
+
+    } elseif ($switch -eq "n") {
+
+        # don't switch
+
+        Write-Host "-------------------------------------------------------------"
+        Write-Host ">> 5: `$switch = n, staying on < $current_branch > branch"
+
+    } else {
+
+        # don't switch
+
+        Write-Host "-------------------------------------------------------------"
+        Write-Host ">> 5: `$switch = [], staying on < $current_branch > branch"
+
     }
     
 }
+
+Write-Host "-------------------------------------------------------------"
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
