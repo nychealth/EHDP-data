3c52cbfc2673fac6c3c04ee85a5e1bb06a5a6f31
cgettings <gettings.chris@gmail.com>
2023-12-29 12:48:54 -0500

    correcting minor oversights

--- export-scripts/NR_sparkbars.py
+++ export-scripts/NR_sparkbars.py
@@ -56,7 +56,7 @@ def chart_fun(ind, df, base_dir, conda_prefix):
 
     # - filter by data field name to create dataset
 
-    dset = df[df.data_field_name == df['data_field_name'][ind]]
+    dset = df[df.indicator_data_name == df['indicator_data_name'][ind]]
     dset = dset.sort_values('data_value')
 
     # - use Altair, the python connector to Vega-Lite
@@ -93,9 +93,9 @@ def chart_fun(ind, df, base_dir, conda_prefix):
 
     chart_json = chart.to_json()
 
-    # - name each SVG with the data_field_name and the Neighborhood
+    # - name each SVG with the indicator_data_name and the Neighborhood
 
-    image_name = base_dir + '/neighborhood-reports/images/' + df['data_field_name'][ind] + '_' + df['geo_join_id'][ind] + '.svg'
+    image_name = base_dir + '/neighborhood-reports/images/' + df['indicator_data_name'][ind] + '_' + df['geo_join_id'][ind] + '.svg'
 
     # use VL CLI program to create SVG
 
@@ -124,7 +124,7 @@ for file in data_files:
     
     print("> ", file)
     
-    df = pd.read_csv(base_dir + "/neighborhood-reports/data/viz/" + file)
+    df = pd.read_json(base_dir + "/neighborhood-reports/data/viz/" + file)
     
     # convert End Date to date data type
     
@@ -138,19 +138,19 @@ for file in data_files:
     # - only the most recent data End Date for each data field
     
     df = df.sort_values('end_date')
-    df = df.drop_duplicates(subset = ['data_field_name', 'geo_join_id'], keep = 'last')
+    df = df.drop_duplicates(subset = ['indicator_data_name', 'geo_join_id'], keep = 'last')
     df = df.sort_values('geo_join_id')
     
     # - converted the integer to string so that I can concatenate to an image title later
     
     df['geo_join_id'] = df['geo_join_id'].astype(str)
     
-    # Step three: for each data_field_name in the data frame, create a graph and write to SVG file
-    # - create a list of distinct data_field_name / Neighborhood s,
+    # Step three: for each indicator_data_name in the data frame, create a graph and write to SVG file
+    # - create a list of distinct indicator_data_name / Neighborhood s,
     
     df = pd.DataFrame(
         df, 
-        columns = ['data_field_name', 'neighborhood', 'data_value', 'geo_join_id']
+        columns = ['indicator_data_name', 'neighborhood', 'data_value', 'geo_join_id']
     )
     
     # - then loop through the list
