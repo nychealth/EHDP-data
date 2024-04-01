50759bd100068a00982ea8f0950e097d3f55f2c0
cgettings <gettings.chris@gmail.com>
2024-04-01 11:34:26 -0400

    add bash translation, plus update readme

---
+++ delete_data_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# deleting data files for merge
+
+data_dirs=("indicators/data" "indicators/metadata" "neighborhood-reports/data/report" "neighborhood-reports/data/viz" "neighborhood-reports/metadata" "neighborhood-reports/images")
+
+# get all the items first
+
+json_to_delete=()
+svg_to_delete=()
+
+for dir in "${data_dirs[@]}"; do
+    json_to_delete+=( $(find "$dir" -maxdepth 1 -type f -name "*.json") )
+    svg_to_delete+=( $(find "$dir" -maxdepth 1 -type f -name "*.svg") )
+done
+
+# now delete all the items
+
+for file in "${json_to_delete[@]}" "${svg_to_delete[@]}"; do
+    rm "$file"
+done
