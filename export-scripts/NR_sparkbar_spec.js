609a018e3e174582f028d9631150ff5a9e01087b
cgettings <gettings.chris@gmail.com>
2024-04-10 13:12:28 -0400

    nodeJS script for producing sparkbar spec

---
+++ export-scripts/NR_sparkbar_spec.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+// const vl = require('vega-lite');
+const vl = require('vega-lite-api');
+
+
+// Reverse-engineering the spec
+
+const chart = vl
+
+    // set mark type
+
+    .markBar()
+
+    // Define the named dataset
+
+    .datasets({
+        the_data: []
+    })
+    .data({name: "the_data"})
+
+    // Set configuration options
+
+    .config({
+        axis: { grid: false },
+        background: 'transparent',
+        view: { continuousHeight: 300, continuousWidth: 300, strokeWidth: 0 }
+    }) 
+
+    // set encoding
+
+    .encode(
+
+        // Encode color
+
+        vl.color().condition({
+            test: "datum.neighborhood == 'Kingsbridge - Riverdale'",
+            value: "#00923E"
+        }).value("#D2D4CE"), 
+
+        // Encode x-axis
+
+        vl.x().axis(null).field('neighborhood').sort('y').type('nominal'),
+
+        // Encode y-axis
+
+        vl.y().axis(null).field('unmodified_data_value_geo_entity').type('quantitative')
+    )
+    
+    // Set height
+
+    .height(100) 
+
+    // Set width
+
+    .width(300); 
+
+
+// convert to spec
+
+const spec = JSON.stringify(chart.toSpec(), null, 4)
+
+
+// File path where JSON file will be saved
+
+const filePath = 'neighborhood-reports/sparkbar_spec.json';
+
+
+// Write JSON data to file
+
+fs.writeFile(filePath, spec, (err) => {
+
+  if (err) {
+
+    console.error('Error writing JSON file:', err);
+
+  } else {
+
+    console.log('JSON file has been saved successfully!');
+
+  }
+
+});
+
+
