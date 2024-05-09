bed1c577311757985e60084fa7c7d03934ac08d1
klane1 <klane1@health.nyc.gov>
2024-05-09 08:51:16 -0400

    Regular auto-commit

--- export-scripts/syndromic_push.r
+++ export-scripts/syndromic_push.r
@@ -27,21 +27,21 @@ system("git pull -X theirs --ff --no-edit")
 heat_syndrome_dir <- "~/networkDrives/smb-share:server=sasshare01,share=sasshare/EHS/BESP/SecuredFolder/Syndromic/Heat_ED/EH data portal/live_data/EHDP-data/datafeatures/heatsyndrome"
 
 # read the updated data
-edheat_live <- read_csv(paste0(heat_syndrome_dir, "/edheat2023_live.csv"))
+edheat_live <- read_csv(paste0(heat_syndrome_dir, "/edheat2024_live.csv"))
 
 #set surveillance window
-start <- as.Date("2023-04-30")
-end   <- as.Date("2023-10-01")
+start <- as.Date("2024-04-30")
+end   <- as.Date("2024-10-01")
 
 # restrict to the surveillance window
 edheat_live2 <- edheat_live[edheat_live$END_DATE > start & edheat_live$END_DATE < end, ]
 
 # save updated data to repo
-write_csv(edheat_live2, "~/EHDP-data/key-topics/heat-syndrome/edheat2023_live.csv")
+write_csv(edheat_live2, "~/EHDP-data/key-topics/heat-syndrome/edheat2024_live.csv")
 
 # add all file changes
 # system("git add .")
-git_add("~/EHDP-data/key-topics/heat-syndrome/edheat2023_live.csv")
+git_add("~/EHDP-data/key-topics/heat-syndrome/edheat2024_live.csv")
 
 # commit with message
 # system("git commit --all --message 'Regular auto-commit'")
