3fc4c4e459d2f0b19869f9a69a06cf9b0732b99d
cgettings <cgettings@health.nyc.gov>
2024-07-15 18:35:49 -0400

    default to production

--- export-scripts/set_environment.bash
+++ export-scripts/set_environment.bash
@@ -237,14 +237,12 @@ export current_branch=$current_branch
 # ask user
 
 echo "-------------------------------------------------------------"
-# read -p "Specify site repo branch (default = $current_branch): " site_branch
 read -p "Specify site repo branch (default = production): " site_branch
 
 # if no value, set to current branch
 
 if [[ -z "$site_branch" ]]; then
 
-    # site_branch=$current_branch
     site_branch='production'
     
 fi
