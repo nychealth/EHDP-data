1c2f259ff3f7881887c3644d21506ccf3d26f846
cgettings <cgettings@health.nyc.gov>
2024-08-29 15:59:52 -0400

    expand and annotate

--- geography/download_shapefiles.R
+++ geography/download_shapefiles.R
@@ -1,57 +1,161 @@
-library(glue)
-library(readr)
-library(purrr)
-library(fs)
-library(stringr)
+###########################################################################################-
+###########################################################################################-
+##
+##  download geo shapefiles
+##
+###########################################################################################-
+###########################################################################################-
+
+#=========================================================================================#
+# Setting up ----
+#=========================================================================================#
+
+#-----------------------------------------------------------------------------------------#
+# Loading libraries
+#-----------------------------------------------------------------------------------------#
+
+suppressWarnings(suppressMessages(library(glue)))
+suppressWarnings(suppressMessages(library(readr)))
+suppressWarnings(suppressMessages(library(purrr)))
+suppressWarnings(suppressMessages(library(fs)))
+suppressWarnings(suppressMessages(library(stringr)))
+
+#-----------------------------------------------------------------------------------------#
+# get and set env vars
+#-----------------------------------------------------------------------------------------#
+
+# find script
+
+set_environment_loc <-
+    list.files(
+        getwd(),
+        pattern = "set_environment.R",
+        full.names = TRUE,
+        recursive = TRUE
+    )
+
+# run script
+
+source(set_environment_loc)
+
+#-----------------------------------------------------------------------------------------#
+# set geo file params
+#-----------------------------------------------------------------------------------------#
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# base url
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
 base_url <- "https://s-media.nyc.gov/agencies/dcp/assets/files/zip/data-tools/bytes"
 
-release <- read_file("geography/release")
 
-geos <- read_file("geography/geos")
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# file names and version
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
+geos    <- read_file(path(base_dir, "geography/geos"))
+release <- read_file(path(base_dir, "geography/release"))
+
+
+#=========================================================================================#
+# data ops ----
+#=========================================================================================#
+
+#-----------------------------------------------------------------------------------------#
 # delete existing files
+#-----------------------------------------------------------------------------------------#
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# get existing
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
 geo_files <- 
-    dir_ls("geography") %>% 
+    dir_ls(path(base_dir, "geography")) %>% 
     keep( ~ str_detect(.x, paste0(geos, collapse = "|")))
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# get release number
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
 geo_files_release <- 
     geo_files %>% 
     str_extract("(_)(\\d{2}[a-z])", group = 2) %>% 
-    unique()
+    max()
 
-# COMPARE TO RELEASE, IF THE SAME, DON'T DO ANYTHING
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# check file version against release
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
-# delete files and folders
+up_to_date <- geo_files_release == release
 
-cat("||", "Delete existing", "||", sep = " ")
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# delete files and folders if not up to date
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
-geo_files %>% path_filter(regexp = "\\.zip") %>% walk( ~ file_delete(.x))
-geo_files %>% path_filter(regexp = "\\.zip", invert = TRUE) %>% walk( ~ dir_delete(.x))
+if (!up_to_date) {
+    
+    # first print a message
+    
+    cat("Updating", geo_files_release, "to", release, "\n")
+    
+    # now actually delete
+    
+    geo_files %>% path_filter(regexp = "\\.zip") %>% walk( ~ file_delete(.x))
+    geo_files %>% path_filter(regexp = "\\.zip", invert = TRUE) %>% walk( ~ dir_delete(.x))
+    
+} 
 
 
-# match geos
+#-----------------------------------------------------------------------------------------#
+# download updated files
+#-----------------------------------------------------------------------------------------#
 
-urls <- glue("{base_url}/{geos}_{release}.zip")
-destfiles <- glue("geography/{geos}_{release}.zip")
+if (!up_to_date) {
 
-# download
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # build arguments to `download.file`
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    
+    # set download URLs
+    
+    urls <- glue("{base_url}/{geos}_{release}.zip")
+    
+    # set downloaded file names
+    
+    destfiles <- path(base_dir, glue("geography/{geos}_{release}.zip"))
+    
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # download
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    
+    download.file(urls, destfiles, method = "libcurl")
+    
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # unzip
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    
+    destfiles %>% walk( ~ unzip(.x, exdir = path(base_dir, "geography")))
+    
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # list zip files
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    
+    zip_files <- 
+        dir_ls(path(base_dir, "geography"), glob = "*.zip") %>% 
+        keep( ~ str_detect(.x, paste0(geos, collapse = "|")))
+    
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # delete zip files
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    
+    zip_files %>% walk( ~ file_delete(.x))
 
-download.file(urls, destfiles, method = "libcurl")
+}
 
-# unzip
-
-destfiles %>% walk( ~ unzip(.x, exdir = "geography"))
-
-# delete zip files
-
-zip_files <- 
-    dir_ls("geography", glob = "*.zip") %>% 
-    keep( ~ str_detect(.x, paste0(geos, collapse = "|")))
-
-# delete files and folders
-
-cat("||", "Delete new zip", "||", sep = " ")
-
-zip_files %>% walk( ~ file_delete(.x))
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# #
+# #                             ---- THIS IS THE END! ----
+# #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
